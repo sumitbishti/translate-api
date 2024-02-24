7471fb9cd04d8e6930ca74aba88e6e86a54a4e36
Sumit Bisht <sumit7beazt@gmail.com>
2024-02-24 15:24:53 +0530

    translate french to english api

---
+++ src/app/api/translate/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import { translate } from '@vitalets/google-translate-api';
+
+export async function POST(req, res) {
+
+    try {
+
+        const { text } = await req.json();
+        if (!text) {
+            return NextResponse.json({ status: 400, error: 'Invalid request body. Missing "text" key.' });
+        }
+
+        const resp = await translate(text, { from: 'en', to: 'fr' });
+        
+        return NextResponse.json({ status: 200, translation: resp.text });
+    } catch (error) {
+        if (error.name === 'TooManyRequestsError') {
+            return NextResponse.json({ status: 429, error: 'Your free Quota has been finished! ' });
+        }
+        console.error(error);
+        return NextResponse.json({ status: 500, error: 'Internal Server Error' });
+    }
+}
